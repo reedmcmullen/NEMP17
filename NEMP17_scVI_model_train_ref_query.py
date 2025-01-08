ab6a73cfa29ca7102d09539995aafb6c3a882767
reedmcmullen <54691534+reedmcmullen@users.noreply.github.com>
2025-01-07 16:14:37 -0800

    Create NEMP17_scVI_model_train_ref_query.py

---
+++ NEMP17_scVI_model_train_ref_query.py
@@ -0,0 +1,14 @@
+#Load required packages and modules.
+import os
+import anndata
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import scanpy as sc
+import scvi
+import seaborn as sns
+import torch
+import random
+from scvi.model.utils import mde
+import skmisc
+
