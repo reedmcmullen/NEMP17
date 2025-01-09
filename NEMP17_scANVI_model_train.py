c8bbb8c530d73dffd315611d0b01cc74cb484f52
reedmcmullen <54691534+reedmcmullen@users.noreply.github.com>
2025-01-08 17:27:00 -0800

    Create NEMP17_scANVI_model_train.py

---
+++ NEMP17_scANVI_model_train.py
@@ -0,0 +1,43 @@
+#! /usr/bin/env python3
+#Import required packages and modules.
+import pandas as pd
+import scanpy as sc
+import anndata as ad
+import matplotlib.pyplot as plt
+import os
+import numpy as np
+
+#Define variables and settings.
+directory_path = '/wynton/home/pollenlab/reedmcmullen/projects/NEMP17/refmap_NEMP17'
+os.chdir(directory_path)
+save_name = 'NEMP17'
+
+#Load the dataset.
+print('Loading dataset...')
+concat_preprocessed = directory_path + '/ref_query_concat_preprocessed.h5ad'
+adata_concat = sc.read_h5ad(concat_preprocessed)
+
+#Load the scVI model.
+print('Loading scVI model...')
+scvi_model = scvi.model.SCVI.load(directory_path + '/concat_scvi_model/', adata_concat)
+
+#Set up labels to transfer.
+print('Setting up dataset...')
+adata_concat.obs["labels_scANVI"] = 'Unknown'
+ref_mask = adata_concat.obs["dataset"] == "reference"
+adata_concat.obs["labels_scANVI"][ref_mask] = adata_concat.obs.age_bracket_by_region_by_cell_class[ref_mask].values
+
+#Set up scANVI model.
+print('Setting up scANVI model...')
+scanvi_model = scvi.model.SCANVI.from_scvi_model(scvi_model, adata=adata_concat, unlabeled_category="Unknown", labels_key='labels_scANVI')
+
+#Training scANVI model.
+print('Training scANVI model...')
+scanvi_model.train(max_epochs=20, n_samples_per_label=100)
+
+#Save scANVI model.
+print('Saving scANVI model...')
+scanvi_model.save(directory_path + '/concat_scanvi_model/', overwrite=True)
+
+
+
